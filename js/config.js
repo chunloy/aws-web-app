3cf2cb485d998f635c48453f51209558a2c9e60c
Aaron Y <101907461+chunloy@users.noreply.github.com>
2022-12-05 11:16:36 -0800

    update cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_3u8EDmnGG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '64p762m7oa53rsio6q3id0lrh4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
